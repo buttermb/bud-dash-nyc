7214e00e8590edefa93cb43e282fc247f63b5d44
buttermb <sake121211@gmail.com>
2025-10-29 10:31:13 -0400

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -1,1 +1,37 @@
-2025/10/28 23:28:31 Access token not provided. Supply an access token by running supabase login or setting the SUPABASE_ACCESS_TOKEN environment variable.
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  __InternalSupabase: {
+    PostgrestVersion: "13.0.5"
+  }
+  public: {
+    Tables: {
+      [key: string]: {
+        Row: Record<string, any>
+        Insert: Record<string, any>
+        Update: Record<string, any>
+        Relationships: any[]
+      }
+    }
+    Views: {
+      [key: string]: {
+        Row: Record<string, any>
+      }
+    }
+    Functions: {
+      [key: string]: {
+        Args: Record<string, any>
+        Returns: any
+      }
+    }
+    Enums: {
+      [key: string]: string
+    }
+  }
+}
